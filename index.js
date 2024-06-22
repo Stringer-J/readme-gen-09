68ada47fd6dbb964c441c6b4fff16ad175ec4dbd
Josh Stringer <joshstringer@live.com>
2024-06-22 08:03:37 -0500

    readme now generates with specific things rather than the entire object

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const writeReadme = (info) => {
   fs.writeFile('READMEtest.md', info, (err) => err ? console.error(err) : console.log('README created'))
 }
 
+
 inquirer
   .prompt([
     {
@@ -58,5 +59,5 @@ inquirer
     }
   ]).then((info) => {
     console.log(info);
-    writeReadme(JSON.stringify(info));
+    writeReadme(info.title);
   })
